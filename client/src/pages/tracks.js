cde3708ffcf5f90ea77a5c178364afdaa7d29e09
Akeem Amusat <aoamusat@gmail.com>
2023-11-29 13:59:27 +0100

    Apollo GraphQL Server

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Layout, QueryResult } from "../components";
+
+/** TRACKS gql query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTrackQuery {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      moduleCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id + index} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
